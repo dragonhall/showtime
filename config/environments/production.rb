8299eccfc0e99900de47e1ee1a5f5f874be6c696
Gabor Garami <hron@hron.me>
2017-08-26 17:11:47 +0200

    Falling back to serve all assets with Rails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,12 +30,14 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = NullCompressor.new
+  # config.assets.js_compressor = NullCompressor.new
   # config.assets.compress = false
-  config.assets.css_compressor = :sass
+  # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # config.assets.compile = false
+  config.assets.debug = true
+  config.assets.quiet = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
