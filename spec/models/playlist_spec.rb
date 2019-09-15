a43972b0ac78c4ec362d302f78c5531d0ed66fd0
Gabor Garami <hron@hron.me>
2019-09-15 07:55:44 +0200

    Updated playlist spec

--- spec/models/playlist_spec.rb
+++ spec/models/playlist_spec.rb
@@ -8,66 +8,101 @@ RSpec.describe Playlist, type: :model do
   let(:group) { create :group, name: 'FullAdmin' }
   let(:admin) do
     create :admin, name: 'Atyauristen',
-                   email: 'admin@example.com',
-                   password: 'secret',
-                   password_confirmation: 'secret',
-                   group_id: group.id
+           email: 'admin@example.com',
+           password: 'secret',
+           password_confirmation: 'secret',
+           group_id: group.id
   end
   let(:video) do
     create :video, path: '/var/film.mpg',
-                   metadata: {
-                     'title' => 'Foobar',
-                     'length' => 45.minutes
-                   },
-                   pegi_rating: 'pegi_3',
-                   video_type: 'film'
+           metadata: {
+               'title' => 'Foobar',
+               'length' => 45.minutes
+           },
+           pegi_rating: 'pegi_3',
+           video_type: 'film'
   end
   let(:intro) do
-    create :video, path: '/var/intro.mpg',
-                   metadata: {
-                     'title' => 'Intro foo',
-                     'length' => 1.minutes
-                   },
-                   pegi_rating: 'pegi_3',
-                   video_type: 'intro'
+    # create :video, path: '/var/intro.mpg',
+    #        metadata: {
+    #            'title' => 'Intro foo',
+    #            'length' => 1.minutes
+    #        },
+    #        pegi_rating: 'pegi_3',
+    #        video_type: 'intro'
+    create :intro
   end
 
-  context :finalize! do
-    let(:channel) do
-      group.save!
-      create :channel
+  let(:channel) do
+    group.save! if group.new_record?
+    create :channel
+  end
+
+  context :wrap_films! do
+    context 'with 1 film' do
+      let(:playlist) do
+        pl = channel.playlists.create! intro_id: intro.id,
+                                       title: 'MyString',
+                                       start_time: 1.hours.from_now
+        pl.tracks.create(video_id: video.id)
+
+        pl
+      end
+
+      let(:wrapped_playlist) do
+        playlist.wrap_films!
+        playlist.reload
+      end
+
+      it 'has 3 tracks' do
+        expect(wrapped_playlist.tracks.count).to equal 3
+      end
+
+      it 'renumbers tracks' do
+        expect(wrapped_playlist.tracks.collect(&:position)).to eq [1, 2, 3]
+      end
+
+      it 'adds intro around the film' do
+        expect(wrapped_playlist.tracks.first.video_id).to eq intro.id
+        expect(wrapped_playlist.tracks.first.position).to eq 1
+
+        expect(wrapped_playlist.tracks.films.first.position).to eq 2
+
+        expect(wrapped_playlist.tracks.last.video_id).to eq intro.id
+        expect(wrapped_playlist.tracks.last.position).to eq 3
+      end
     end
 
-    let(:playlist) do
-      pl = channel.playlists.create! intro_id: intro.id,
-                                     title: 'MyString',
-                                     start_time: 1.hours.from_now
-      pl.tracks.create(video_id: video.id)
+    context 'with 2 films' do
+      let :playlist do
+        pl = channel.playlists.create! intro_id: intro.id, title: 'MyString 2', start_time: 2.hours.from_now
+        videos = [0, 1].map { create :film }
+        videos.each { |video| pl.tracks.create(video_id: video.id) }
 
-      pl
-    end
+        pl
+      end
 
-    let(:finalized_playlist) do
-      playlist.finalize! unless playlist.finalized?
-      playlist.reload
-    end
+      let :wrapped_playlist do
+        playlist.wrap_films!
+        playlist.reload
+      end
 
-    it 'has 3 tracks' do
-      expect(finalized_playlist.tracks.count).to equal 3
-    end
+      it 'has 6 tracks' do
+        expect(wrapped_playlist.tracks.count).to equal 6
+      end
 
-    it 'renumbers tracks' do
-      expect(finalized_playlist.tracks.collect(&:position)).to eq [1, 2, 3]
-    end
+      it 'adds intro around the films' do
+        # @type Video[]
+        videos = wrapped_playlist.tracks.collect(&:video)
 
-    it 'adds intro around the film' do
-      expect(finalized_playlist.tracks.first.video_id).to eq intro.id
-      expect(finalized_playlist.tracks.first.position).to eq 1
-
-      expect(finalized_playlist.tracks.films.first.position).to eq 2
-
-      expect(finalized_playlist.tracks.last.video_id).to eq intro.id
-      expect(finalized_playlist.tracks.last.position).to eq 3
+        expect(videos[0].id).to eq intro.id
+        expect(videos[1].id).to_not eq intro.id
+        expect(videos[1].video_type).to eq 'film'
+        expect(videos[2].id).to eq intro.id
+        expect(videos[3].id).to eq intro.id
+        expect(videos[4].video_type).to eq 'film'
+        expect(videos[5].id).to eq intro.id
+      end
     end
   end
 end
