42b38afdc0a3b101f3d105454d7f6de9e9b65b04
Gabor Garami <hron@hron.me>
2019-09-22 14:29:55 +0200

    Fixing Redis/Resque setup
    Resque/Redis did not get custom Redis urls from config

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -3,13 +3,20 @@ require 'resque/server'
 require 'resque/scheduler/server'
 require 'active_scheduler'
 
-redis_url = YAML.load(Rails.root.join('config', 'redis.yml'))[Rails.env.to_s] rescue 'redis://localhost:6379/0'
+redis_url = YAML.load_file(Rails.root.join('config', 'redis.yml'))[Rails.env.to_s] # rescue 'redis://localhost:6379/0'
 
 REDIS_APPKEY = Rails.application.class.name.underscore.split('/').first
 
+puts " >> Initializing Redis at #{redis_url}"
 # Resque.redis = Redis::Namespace.new(REDIS_APPKEY.to_sym, redis: Redis::Client.new(url: redis_url))
-Redis.current = Redis::Namespace.new(REDIS_APPKEY.to_sym, redis: Redis::Client.new(url: redis_url))
+#redis = Redis::Namespace.new(REDIS_APPKEY.to_sym, redis: Redis::Client.new(url: redis_url))
+redis = Redis.new(url: redis_url)
+redis_ns = Redis::Namespace.new(REDIS_APPKEY.to_sym, redis: redis)
+
+Redis.current = redis_ns
 
 yaml_schedule    = YAML.load_file(Rails.root.join('config', 'resque_schedule.yaml')) || {}
 wrapped_schedule = ActiveScheduler::ResqueWrapper.wrap yaml_schedule
+
+Resque.redis     = redis_ns
 Resque.schedule  = wrapped_schedule
