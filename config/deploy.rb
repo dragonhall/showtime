986b0b91b0e09c8bd2c79ab36bbefd532f3daa59
Gabor Garami <hron@hron.me>
2019-12-16 19:02:38 +0100

    Add redis.yml to deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :format_options, command_output: true, truncate: false, color: :auto
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, 'config/database.yml', 'config/secrets.yml'
+append :linked_files, 'config/database.yml', 'config/secrets.yml', 'config/redis.yml'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log',
